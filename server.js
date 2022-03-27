a7ba41ceac13d7db7983469a84c21ec5f8ea2ad4
eli <eliyahu280@gmail.com>
2022-03-27 07:53:16 +0300

    swagget

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ if (process.env.NODE_ENV == "development") {
         definition: {
             openapi: "3.0.0",
             info: {
-                title: "Node Demo API",
+                title: "Node API Description",
                 version: "1.0.0",
-                description: "A simple Express Library API",
+                description: "Explanatin about API",
             },
             servers: [{url: "http://localhost:" + process.env.PORT,},],
         },
