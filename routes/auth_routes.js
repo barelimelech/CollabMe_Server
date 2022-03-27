a7191d1d64b9adb2ffe0585fca2ecd597feb6f31
eli <eliyahu280@gmail.com>
2022-03-27 07:53:40 +0300

    swagger

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -3,8 +3,15 @@ const router = express.Router()
 
 const Auth = require('../controllers/auth')
 
-
+/**
+* @swagger
+* tags:
+*   name: User Authentication
+*   description: Info User authentication
+*/
 router.post('/login', Auth.login)
+
+
 router.post('/register', Auth.register)
 router.post('/logout', Auth.logout)
 router.get('/refreshToken', Auth.refreshToken)
