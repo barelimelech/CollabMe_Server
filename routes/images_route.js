75073033429081a939a8dc8a014a621805986847
amit ben basat <amitbasat124@gmail.com>
2022-03-31 16:09:38 +0300

    adding the username to the image route and adding the image to the users

--- routes/images_route.js
+++ routes/images_route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express();
-const images = require('../controllers/images')
+const user = require('../controllers/users')
+
 var multer, storage, path, crypto;
 multer = require('multer')
 path = require('path');
 crypto = require('crypto');
+var fs = require('fs');
+
+
 router.use('/uploads', express.static('uploads'));
 storage = multer.diskStorage({
   destination: './uploads/',
@@ -20,13 +24,28 @@ storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 router.post(
-  "/upload",
+  "/upload/:username",
   multer({
     storage: storage
   }).single('upload'), function(req, res) {
+    console.log(req.file);
+    console.log(req.params.username); 
     res.redirect(200,"/uploads/" + req.file.filename);
-    res.status(200).send();
+    res.status(200).send("/uploads/" + req.file.filename);
   });
+
+
+  /*
+  router.get('/uploads/:upload', function (req, res){
+    file = req.params.upload;
+    console.log(req.params.upload);
+    var img = fs.readFileSync(__dirname + "/uploads/" + file);
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
+  
+  });
+  */
+  
 
  
 module.exports = router
