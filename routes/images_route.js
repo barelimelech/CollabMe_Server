cc41657597acab88f6b6af171727e52de6e34ec9
eli <eliyahu280@gmail.com>
2022-04-25 18:57:22 +0300

    swagger image (partial)

--- routes/images_route.js
+++ routes/images_route.js
@@ -8,6 +8,35 @@ path = require('path');
 crypto = require('crypto');
 var fs = require('fs');
 
+/**
+* @swagger
+* tags:
+*   name: Images Api
+*   description: The Images API
+*/
+
+// /**
+// * @swagger
+// * /image/uploads:
+// *   post:
+// *     summary: ______
+// *     tags: [Images Api]
+// *     parameters:
+// *       - in: path
+// *         name: path  Image
+// *         schema:
+// *           type: string
+// *         required: true
+// *         description: the path to the file that store the image
+// *     responses:
+// *       200:
+// *         description: 
+// *         content:
+// *           application/json:
+// *             schema:
+// *               type: string
+// */
+
 router.use('/uploads', express.static('uploads'));
 storage = multer.diskStorage({
   destination: './uploads/',
@@ -22,7 +51,28 @@ storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage })
 
-
+/**
+* @swagger
+* /image/upload:
+*   post:
+*     summary: uploading image (image format) and convert to url
+*     tags: [Images Api]
+*     consumes:
+*       - multipart/form-data
+*     parameters:
+*       - in: formData
+*         name: upfile
+*         type: file
+*         required: true
+*         description: The image file to upload.
+*     responses:
+*       200:
+*         description: image was uploaded seccessfully
+*         content:
+*           application/json:
+*             schema:
+*               type: string
+*/
 
 router.post("/upload", upload.single("upload"), async (req, res) => {
   if (req.file === undefined) return res.status(400).send;
@@ -30,6 +80,33 @@ router.post("/upload", upload.single("upload"), async (req, res) => {
    res.status(200).send(imgUrl);
 });
 
+/**
+* @swagger
+* /image/file/{filename}:
+*   get:
+*     summary: get URL and send it as byte
+*     tags: [Images Api]
+*     parameters:
+*       - in: path
+*         name: URL
+*         schema:
+*           type: string
+*         required: true
+*         description: URL of an image
+*     responses:
+*       200:
+*         description: ok
+*         Content:
+*           application/json:
+*            schema: 
+*              type: string
+*/
+
+// type: object
+// *       properties:
+// *         Description:
+// *           type: string
+// *           description: description of the offer
 
 router.get("/file/:filename", async (req, res) => {
   let nulli = req.params.filename.toString();
@@ -40,6 +117,7 @@ router.get("/file/:filename", async (req, res) => {
     try{
       res.writeHead(200, {'Content-Type': 'image/jpeg'});
       res.end(data); // Send the file data to the browser.  
+      console.log('was here')
     }catch(err){
       res.status(400).send();
     }
